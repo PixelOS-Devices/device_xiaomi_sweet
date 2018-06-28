4199ebc8f288734e3578bbbf8510c7bff008acd5
Christian Oder <myself5@carbonrom.org>
2018-06-27 21:14:43 -0400

    sweet: avb: Build disabled VBMeta image
    Change-Id: I381feef8f6fefc8449ca50d85d704b67bcc8a77e

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,3 +81,8 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 
 # Treble
 BOARD_VNDK_VERSION := current
+
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 2
