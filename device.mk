4ca081550e7ceb7f25872d926105e2244caada3c
TheScarastic <warabhishek@gmail.com>
2018-11-15 09:12:42 -0500

    sweet: Add Xiaomi's vendor platform signature
    Change-Id: I727bbad29ca86723d4e1be2004c98e5e99b05497

--- device.mk
+++ device.mk
@@ -70,7 +70,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 
 # rro_overlays
 PRODUCT_PACKAGES += \
