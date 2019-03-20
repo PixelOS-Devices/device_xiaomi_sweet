458e98892a8cbaebfc3fd9ba1f88c959a0164b7f
Jenhao Chen <jenhaochen@google.com>
2019-03-20 14:57:51 -0700

    sweet: enable zram writeback
    Bug: 128889899
    Test: Boot to home
    
    Change-Id: I48ae06e54a66a7d6965afbe8db3d2f157cf8a1f4

--- system_prop.mk
+++ system_prop.mk
@@ -9,3 +9,9 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aptxadaptive-aac-ldac \
     persist.vendor.btstack.enable.splita2dp=true \
     vendor.bluetooth.soc=cherokee
+    
+# ZRAM writeback
+PRODUCT_PROPERTY_OVERRIDES += \
+ro.zram.mark_idle_delay_mins=60 \
+ro.zram.first_wb_delay_mins=180 \
+ro.zram.periodic_wb_delay_hours=24
