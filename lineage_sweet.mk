fbf1319641771442db9aae328b548422d552644f
AbhiShek Aggarwal <warabhishek@gmail.com>
2019-08-26 17:40:14 -0400

    sweet: Add custom init to set proper variants
    Co-authored-by: Arian <arian.kulmer@web.de>
    Change-Id: I04f09a14cf41c89b3135c53e3089d5c3e703aae0

--- lineage_sweet.mk
+++ lineage_sweet.mk
@@ -17,11 +17,4 @@ PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10 Pro
 PRODUCT_MANUFACTURER := Xiaomi
 
-# Build info
-BUILD_FINGERPRINT := "Redmi/sweetin/sweetin:11/RKQ1.200826.002/V12.0.1.0.RKFINXM:user/release-keys"
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=sweetin \
-    PRODUCT_NAME=sweetin \
-    PRIVATE_BUILD_DESC="sweetin-user 11 RKQ1.200826.002 V12.0.1.0.RKFINXM release-keys"
-
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
