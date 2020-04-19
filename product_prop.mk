a8dfb89cc01c434fd924ba86e077ede22799c33c
Sultan Alsawaf <sultan@kerneltoast.com>
2020-04-18 23:20:50 -0700

    sweet: props: Force triple frame buffers
    This yields significantly reduced jitter and doesn't have any measurable
    impact on latency.
    
    Change-Id: I4d1de21487757be3df3bd908e1a6152fd951d732

--- product_prop.mk
+++ product_prop.mk
@@ -10,7 +10,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
 	debug.sf.disable_backpressure=1 \
-	debug.sf.enable_hwc_vds=1
+	debug.sf.enable_hwc_vds=1 \
+	ro.surface_flinger.max_frame_buffer_acquired_buffers=3
 
 # LMKD
 PRODUCT_PRODUCT_PROPERTIES += \
