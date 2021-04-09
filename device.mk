67522dd66db491d6d25cb3e3386f0c5ba3a51759
AdarshGrewal <adarsh@aosip.dev>
2021-04-09 15:32:05 +0530

    sweet: disable xiaomi motor

--- device.mk
+++ device.mk
@@ -40,6 +40,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
+# Init
+PRODUCT_PACKAGES += \
+    init.qcom.rc
+
 # Partitions 
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 PRODUCT_BUILD_SUPER_PARTITION := false
