01b639d922edb8ee8ca3f18869b795db86b9ce98
AdarshGrewal <adarsh@aosip.dev>
2021-04-10 11:16:28 +0530

    sweet: include dalvik heap config

--- device.mk
+++ device.mk
@@ -14,6 +14,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
+# setup dalvik vm configs.
+$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
+
 # The first api level, device has been commercially launched on.
 PRODUCT_SHIPPING_API_LEVEL := 30
 
