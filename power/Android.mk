466e2a32679e131d6e1165e143511a2cfc3af03c
AdarshGrewal <adarsh@aosip.dev>
2021-04-20 15:28:43 +0530

    sweet: power: import Power aidl from LA.UM.9.1.r1-09200-SMxxx0.0  * build and vendor overlay it

---
+++ power/Android.mk
@@ -0,0 +1,26 @@
+LOCAL_PATH := $(call my-dir)
+
+
+# HAL module implemenation stored in
+# hw/<POWERS_HARDWARE_MODULE_ID>.<ro.hardware>.so
+include $(CLEAR_VARS)
+LOCAL_MODULE := android.hardware.power-service.sweet
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/bin
+LOCAL_MODULE_RELATIVE_PATH := hw
+LOCAL_MODULE_STEM := android.hardware.power-service
+
+LOCAL_REQUIRED_MODULES := android.hardware.power-service.rc
+LOCAL_SHARED_LIBRARIES := liblog libcutils libdl libxml2 libbase libbinder_ndk libutils android.hardware.power-ndk_platform
+LOCAL_HEADER_LIBRARIES += libutils_headers
+LOCAL_HEADER_LIBRARIES += libhardware_headers
+LOCAL_SRC_FILES := power-common.c metadata-parser.c utils.c list.c hint-data.c powerhintparser.c main.cpp Power.cpp
+LOCAL_C_INCLUDES := external/libxml2/include \
+                    external/icu/icu4c/source/common
+
+ifeq ($(TARGET_USES_INTERACTION_BOOST),true)
+    LOCAL_CFLAGS += -DINTERACTION_BOOST
+endif
+
+include $(BUILD_EXECUTABLE)
