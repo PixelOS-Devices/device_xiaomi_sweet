4ac17839d5cb2fa0c883b6099e16c3a19064fa2c
Ethan Chen <intervigil@gmail.com>
2021-04-25 13:06:00 +0200

    sweet: Use Soong namespace to avoid device build conflicts
    Change-Id: I5e2375725b5104d9eb502b648032877c7ed5dea6

--- device.mk
+++ device.mk
@@ -135,6 +135,10 @@ TARGET_USES_MKE2FS := true
 -include $(LOCAL_PATH)/system_prop.mk
 PRODUCT_COMPATIBLE_PROPERTY_OVERRIDE := true
 
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
