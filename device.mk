98ef6cf44af80c52f7753de31e81bf35a8489c25
Artem Borisov <dedsa2002@gmail.com>
2021-05-07 17:57:06 +0530

    sweet: Add QCOM's WFD implementation
    * Sadly, out-of-the-box Miracast support was killed in P, so
      we need this to keep Wi-Fi display feature working.
    
    Change-Id: Ieff24313d55a60ca1898430113f0a8b00acfc6a0

--- device.mk
+++ device.mk
@@ -127,3 +127,15 @@ PRODUCT_BOOT_JARS += \
 # VINTF
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/vintf/android.hardware.lights-qti.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/vintf/manifest/android.hardware.lights-qti.xml
+
+# WiFi Display
+PRODUCT_PACKAGES += \
+    libdisplayconfig.qti \
+    libnl \
+    libqdMetaData \
+    libavservices_minijail \
+    libavservices_minijail_32 \
+    vendor.display.config@2.0
+
+PRODUCT_BOOT_JARS += \
+    WfdCommon
