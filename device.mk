99afd504130efdcf52f9d6891c4698ed60eea4db
AdarshGrewal <adarsh@aosip.dev>
2021-05-15 16:34:34 +0530

    sweet: drop aosp dalvik heep configs

--- device.mk
+++ device.mk
@@ -14,9 +14,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 # Enable updating of APEXes
 $(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
 
-# setup dalvik vm configs.
-$(call inherit-product, frameworks/native/build/phone-xhdpi-6144-dalvik-heap.mk)
-
 # Get non-open-source specific aspects
 $(call inherit-product, vendor/xiaomi/sweet/sweet-vendor.mk)
 
