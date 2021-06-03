a7446469a902b7cbcc88080517343ce7e7273cfa
AdarshGrewal <adarsh@aosip.dev>
2021-06-03 11:22:27 +0530

    sweet: props: set call volume default level
     * Also bump steps to match the stock value

--- product_prop.mk
+++ product_prop.mk
@@ -22,7 +22,8 @@ PRODUCT_PRODUCT_PROPERTIES += \
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.media_vol_default=10 \
 	ro.config.media_vol_steps=15 \
-	ro.config.vc_call_vol_steps=6
+	ro.config.vc_call_vol_default=7 \
+	ro.config.vc_call_vol_steps=11
 
 # RIL
 PRODUCT_PROPERTY_OVERRIDES += \
