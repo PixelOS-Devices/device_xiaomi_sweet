781c8b2822538adf61385668068a7e5c72007b5e
AdarshGrewal <adarshgrewal@gmail.com>
2021-08-11 10:53:03 +0530

    sweet: config: import stock WCNSS config

--- device.mk
+++ device.mk
@@ -162,6 +162,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_BOOT_JARS += \
     telephony-ext
 
+# WiFi
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # WiFi Display
 PRODUCT_PACKAGES += \
     libdisplayconfig.qti \
