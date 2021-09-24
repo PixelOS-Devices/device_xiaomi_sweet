1eb4c41a9b96c4fd7d598784b851a8597a76ff4c
AdarshGrewal <adarshgrewal@gmail.com>
2021-09-24 16:23:07 +0530

    sweet: init: Add support for JAPAN region
    Change-Id: I84d07e2de321839d9d9adfe42d351658ddb6f409

--- init/init_sweet.cpp
+++ init/init_sweet.cpp
@@ -100,13 +100,19 @@ void vendor_load_properties() {
             description = "sweetin-user 11 RKQ1.200826.002 V12.0.6.0.RKFINXM release-keys";
             mod_device = "sweetin_in_global";
         } else {
-                model = "M2101K6I";
-                device = "sweetin";
-                fingerprint = "Redmi/sweetinpro/sweetin:11/RKQ1.200826.002/V12.0.6.0.RKFINXM:user/release-keys";
-                description = "sweetinpro-user 11 RKQ1.200826.002 V12.0.6.0.RKFINXM release-keys";
-                mod_device = "sweetin_in_global";
+            model = "M2101K6I";
+            device = "sweetin";
+            fingerprint = "Redmi/sweetinpro/sweetin:11/RKQ1.200826.002/V12.0.6.0.RKFINXM:user/release-keys";
+            description = "sweetinpro-user 11 RKQ1.200826.002 V12.0.6.0.RKFINXM release-keys";
+            mod_device = "sweetin_in_global";
             }
-        }
+    } else if (region == "JAPAN") {
+        model = "M2101K6R";
+        device = "sweet";
+        fingerprint = "Redmi/sweet_global/sweet:11/RKQ1.200826.002/V12.5.4.0.RKFMIXM:user/release-keys";
+        description = "sweet_global-user 11 RKQ1.200826.002 V12.5.4.0.RKFMIXM release-keys";
+        mod_device = "sweet_global";
+    }
 
     set_ro_build_prop("fingerprint", fingerprint);
     set_ro_product_prop("device", device);
