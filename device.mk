e2a245eec689691bea3f75194b3632342a4d68a2
AdarshGrewal <adarshgrewal@gmail.com>
2021-10-05 16:14:08 +0530

    sweet: target VNDK extra 30

--- device.mk
+++ device.mk
@@ -21,6 +21,7 @@ $(call inherit-product, vendor/xiaomi/sweet/sweet-vendor.mk)
 PRODUCT_SHIPPING_API_LEVEL := 30
 
 PRODUCT_TARGET_VNDK_VERSION := 30
+PRODUCT_EXTRA_VNDK_VERSIONS := 30
 
 # A/B
 AB_OTA_UPDATER := false
