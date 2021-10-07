53de9f425e4c6cae72c594f573372e9d71c6880a
AdarshGrewal <adarshgrewal@gmail.com>
2021-10-07 21:27:21 +0530

    sweet: Disable blurs, under dev-option
    This reverts commit d18c3fb0d804498345b1f8bc283b691df3420c40.

--- system_prop.mk
+++ system_prop.mk
@@ -9,9 +9,3 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aptxadaptive-aac-ldac \
     persist.vendor.btstack.enable.splita2dp=true \
     vendor.bluetooth.soc=cherokee
-
-# Enable blurs, hidden under dev option
-PRODUCT_PRODUCT_PROPERTIES += \
-    ro.surface_flinger.supports_background_blur=1 \
-    persist.sys.sf.disable_blurs=1 \
-    ro.sf.blurs_are_expensive=1  
