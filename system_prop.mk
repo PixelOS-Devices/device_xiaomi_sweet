7d219d37a78a60a2167fadeb13dff2b5a88d5d10
kubersharma001 <kubersharma001@gmail.com>
2021-11-13 18:52:14 +0530

    sweet: props: Set frame rate multiple threshold to 120
    Also fixes the Netflix 120hz stuck issue as mentioned
    on Change-Id: Icdd392332a6f44d5d1b2dc1e6e654b11f9e95da2
    
    test: Play a Netflix or YT video; it goes down to lowest refresh rate.
    
    Change-Id: Ie4d5c8c9a45079306ad51e1607ae72eb9493ee0c

--- system_prop.mk
+++ system_prop.mk
@@ -9,7 +9,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.vendor.btstack.a2dp_offload_cap=sbc-aptx-aptxtws-aptxhd-aptxadaptive-aac-ldac \
     persist.vendor.btstack.enable.splita2dp=true \
     vendor.bluetooth.soc=cherokee
-    
+
+# Display
+PRODUCT_PROPERTY_OVERRIDES += \
+debug.sf.frame_rate_multiple_threshold=120
+
 # ZRAM writeback
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.zram.mark_idle_delay_mins=60 \
@@ -19,3 +23,4 @@ ro.zram.periodic_wb_delay_hours=24
 # Pre-rendering
 PRODUCT_PROPERTY_OVERRIDES += \
 ro.vendor.perf.scroll_opt=true
+
