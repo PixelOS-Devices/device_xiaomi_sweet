e2970210fbd41e1f2103dff6db4e8f15d1be2cd8
Zinadin Zidan <zidan44@pixelexperience.org>
2022-01-22 13:17:46 +0600

    sweet: prop: Set media vol steps to 25
    Signed-off-by: Zinadin Zidan <zidan44@pixelexperience.org>
    Change-Id: I74c95b84b3685bd42b51b41decc7416214224b1d

--- product_prop.mk
+++ product_prop.mk
@@ -31,7 +31,7 @@ PRODUCT_PRODUCT_PROPERTIES += \
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
 	ro.config.media_vol_default=10 \
-	ro.config.media_vol_steps=15 \
+	ro.config.media_vol_steps=25 \
 	ro.config.vc_call_vol_default=7 \
 	ro.config.vc_call_vol_steps=11
 
