d87f3a3b562351f78d1b6021b8dce4be8295e7ef
basamaryan <basam.aryan@gmail.com>
2022-04-01 12:04:55 -0400

    sweet: Adapt to PixelOS
    Change-Id: I489b68507c7cecea75645a556ad1c13f442381be

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,9 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_sweet.mk
+    $(LOCAL_DIR)/aosp_sweet.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_sweet-user \
-    lineage_sweet-userdebug \
-    lineage_sweet-eng
+    aosp_sweet-user \
+    aosp_sweet-userdebug \
+    aosp_sweet-eng
