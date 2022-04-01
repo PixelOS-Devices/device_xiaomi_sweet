d504eca240975a2ecfd48a17d250980397fb36ad
basamaryan <basam.aryan@gmail.com>
2022-04-01 12:04:55 -0400

    sweet: Welcome PixelOS
    Change-Id: I489b68507c7cecea75645a556ad1c13f442381be

--- aosp_sweet.mk
+++ aosp_sweet.mk
@@ -11,10 +11,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sweet device
 $(call inherit-product, device/xiaomi/sweet/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common PixelOS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+TARGET_GAPPS_ARCH := arm64
+TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_sweet
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_sweet
+
 PRODUCT_DEVICE := sweet
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi Note 10 Pro
