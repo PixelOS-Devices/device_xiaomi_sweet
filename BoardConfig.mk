9d6e88d91bec33998b0c484a739bd1fdf0d36291
basamaryan <basam.aryan@gmail.com>
2022-04-01 12:04:55 -0400

    sweet: Welcome PixelOS
    Change-Id: I489b68507c7cecea75645a556ad1c13f442381be

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -26,7 +26,7 @@ PRODUCT_FS_COMPRESSION := 1
 
 # HIDL
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/device_framework_compatibility_matrix.xml
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/lineage/config/device_framework_matrix.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += vendor/aosp/config/device_framework_matrix.xml
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/manifest.xml
 ODM_MANIFEST_SKUS += sweet
 ODM_MANIFEST_SWEET_FILES := \
