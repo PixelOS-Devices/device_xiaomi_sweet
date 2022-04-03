6a89a70825576bd842b19377bde557b2668373da
AdarshGrewal <adarshgrewal@gmail.com>
2022-04-03 19:18:28 +0530

    sweet: Include MiuiCamera if exists
    Change-Id: Iee675b4924cb2be7ac2d36c7737b06edc9cadfc9

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -7,6 +7,9 @@
 # Inherit from sm6150-common
 include device/xiaomi/sm6150-common/BoardConfigCommon.mk
 
+# Inherit from proprietary files for miuicamera
+-include vendor/xiaomi/sweet-miuicamera/products/board.mk
+
 DEVICE_PATH := device/xiaomi/sweet
 
 # Assert
