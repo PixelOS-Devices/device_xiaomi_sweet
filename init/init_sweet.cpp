ed21a047ae46f728aaa42c9f742177b4947d57b3
basamaryan <basam.aryan@gmail.com>
2022-04-26 21:38:01 -0400

    sweet: Update build fingerprints from MIUI 13

--- init/init_sweet.cpp
+++ init/init_sweet.cpp
@@ -88,22 +88,22 @@ void vendor_load_properties() {
     if (region == "GLOBAL") {
         model = "M2101K6G";
         device = "sweet";
-        fingerprint = "Redmi/sweet_eea/sweet:11/RKQ1.200826.002/V12.0.10.0.RKFEUXM:user/release-keys";
-        description = "sweet_eea-user 11 RKQ1.200826.002 V12.0.10.0.RKFEUXM release-keys";
+        fingerprint = "Redmi/sweet_eea/sweet:12/SKQ1.210908.001/V13.0.6.0.SKFEUXM:user/release-keys";
+        description = "sweet_eea-user 12 SKQ1.210908.001 V13.0.6.0.SKFEUXM release-keys";
         mod_device = "sweet_eea_global";
         property_override("ro.boot.product.hardware.sku", "sweet");
     } else if (region == "INDIA") {
         if (sku == "std") {
             model = "M2101K6P";
             device = "sweetin";
-            fingerprint = "Redmi/sweetin/sweetin:11/RKQ1.200826.002/V12.0.6.0.RKFINXM:user/release-keys";
-            description = "sweetin-user 11 RKQ1.200826.002 V12.0.6.0.RKFINXM release-keys";
+            fingerprint = "Redmi/sweetin/sweetin:12/SKQ1.210908.001/V13.0.3.0.SKFINXM:user/release-keys";
+            description = "sweetin-user 12 SKQ1.210908.001 V13.0.3.0.SKFINXM release-keys";
             mod_device = "sweetin_in_global";
         } else {
                 model = "M2101K6I";
                 device = "sweetin";
-                fingerprint = "Redmi/sweetinpro/sweetin:11/RKQ1.200826.002/V12.0.6.0.RKFINXM:user/release-keys";
-                description = "sweetinpro-user 11 RKQ1.200826.002 V12.0.6.0.RKFINXM release-keys";
+                fingerprint = "Redmi/sweetinpro/sweetin:12/SKQ1.210908.001/V13.0.3.0.SKFINXM:user/release-keys";
+                description = "sweetinpro-user 12 SKQ1.210908.001 V13.0.3.0.SKFINXM release-keys";
                 mod_device = "sweetin_in_global";
             }
         }
