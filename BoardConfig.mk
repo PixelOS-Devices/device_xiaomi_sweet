1a136396206a9377df07188986d06f4488f04165
basamaryan <basam.aryan@gmail.com>
2022-06-29 03:59:14 +0530

    sweet: Update blobs from MIUI V13.0.10.0.SKFEUXM
    Change-Id: I08abd3c36084cf420bce6d5dea27db0c5e562408

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -93,7 +93,7 @@ TARGET_SCREEN_DENSITY := 440
 BOARD_VENDOR_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy/vendor
 
 # Vendor security patch level
-VENDOR_SECURITY_PATCH := 2022-05-01
+VENDOR_SECURITY_PATCH := 2022-06-01
 
 # Inherit from proprietary files
 include vendor/xiaomi/sweet/BoardConfigVendor.mk
