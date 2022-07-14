30b40bcdddc718d1ecf4ccebb9b4df17f60171e5
basamaryan <basam.aryan@gmail.com>
2022-07-14 23:11:54 +0530

    sweet: configs: Import 0b qdcm display calibration from MIUI V13.0.10.0.SKFEUXM
    * Fix NumModes in qdcm profiles
    * Remove video modes
    * Default to smart_MC
    
    Change-Id: Id6a3691418604f1ef0af714613ab5581f358c129

--- device.mk
+++ device.mk
@@ -30,7 +30,8 @@ PRODUCT_PACKAGES += \
 
 # Display
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml
+    $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0c_0a_fhd_dsc_video_dsi_panel.xml \
+    $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_k6_38_0e_0b_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0e_0b_fhd_dsc_video_dsi_panel.xml
 
 PRODUCT_PACKAGES += \
     libgui_vendor \
