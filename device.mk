cd2dc06f8f4afff0cede0e73d90c5bcf27748126
basamaryan <basam.aryan@gmail.com>
2022-07-21 01:52:23 +0530

    Revert "sweet: touch: add IHighTouchPollingRate"
    This reverts commit 1fb845685dbaa2e6b16989a36dc3b0abce051e89.
    
    Change-Id: Ib289ba2a0c7b2ed081daae9c9d96464985131dec

--- device.mk
+++ device.mk
@@ -123,10 +123,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
-# Touchscreen
-PRODUCT_PACKAGES += \
-    vendor.lineage.touch@1.0-service.sweet
-
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
