3d2a8e87c9df0d1dbcce12df7a5521f537341e9b
daniml3 <daniel@danielml.dev>
2022-07-20 20:59:23 +0200

    sweet: shim stock libsdmcore.so
    * This shim overrides the MIUI's panel dimensions (which are divided
      by 10) so that our libsdmcore behaves in the same way as Qualcomm's
      one
    
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: I19f6358fca8f3a624353ffe624438cf501db4d5a

--- device.mk
+++ device.mk
@@ -34,7 +34,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/display/qdcm_calib_data_xiaomi_k6_38_0e_0b_fhd_dsc_video_dsi_panel.xml:$(TARGET_COPY_OUT_VENDOR)/etc/qdcm_calib_data_xiaomi_k6_38_0e_0b_fhd_dsc_video_dsi_panel.xml
 
 PRODUCT_PACKAGES += \
-    libgui_vendor
+    libgui_vendor \
+    libsdmcore-shim
 
 # Fstab
 PRODUCT_COPY_FILES += \
