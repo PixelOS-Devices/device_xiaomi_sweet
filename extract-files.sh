3b140240bb217e983dd34f7266c8e6074f9ac036
daniml3 <daniel@danielml.dev>
2022-08-19 13:56:19 +0200

    sweet: Use GetPreviewImageData shim for com.qti.node.watermark
    Signed-off-by: daniml3 <daniel@danielml.dev>
    Change-Id: I92e51fdf1dc7d877c7affafbf9803ba5d504be07

--- extract-files.sh
+++ extract-files.sh
@@ -13,6 +13,9 @@ function blob_fixup() {
             sed -i "s|libMegviiFacepp-0.5.2.so|libFaceDetectpp-0.5.2.so|g" "${2}"
             sed -i "s|megviifacepp_0_5_2_model|facedetectpp_0_5_2_model|g" "${2}"
             ;;
+        vendor/lib64/camera/components/com.qti.node.watermark.so)
+            "${PATCHELF}" --add-needed "libpiex_shim.so" "${2}"
+            ;;
     esac
 }
 
