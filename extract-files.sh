b4ba27bb49f9f771364da822ba1013237958f6af
basamaryan <basam.aryan@gmail.com>
2022-10-04 10:16:09 +0000

    fixup! sweet: Rename camera face detection libs
    Change-Id: I3c896665bcb4be461126c2b619e899904fd5f99f

--- extract-files.sh
+++ extract-files.sh
@@ -8,7 +8,7 @@
 
 function blob_fixup() {
     case "${1}" in
-        vendor/lib64/hw/camera.qcom.so | vendor/lib64/libmegface.so | vendor/lib64/libFaceDetectpp-0.5.2.so)
+        vendor/lib64/hw/camera.qcom.so | vendor/lib64/libFaceDetectpp-0.5.2.so | vendor/lib64/libfacedet.so)
             sed -i "s|libmegface.so|libfacedet.so|g" "${2}"
             sed -i "s|libMegviiFacepp-0.5.2.so|libFaceDetectpp-0.5.2.so|g" "${2}"
             sed -i "s|megviifacepp_0_5_2_model|facedetectpp_0_5_2_model|g" "${2}"
